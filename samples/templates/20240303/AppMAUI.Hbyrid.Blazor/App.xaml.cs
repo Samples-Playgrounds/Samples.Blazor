3cb4ebf348e48ee19a787d8621e5bc62521121ba
moljac <mcvjetko@holisticware.net>
2024-04-15 09:42:45 +0200

    testing templates

---
+++ samples/templates/20240303/AppMAUI.Hbyrid.Blazor/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AppMAUI.Hbyrid.Blazor;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ samples/templates/20240303/AppMAUI.Hbyrid.Blazor/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AppMAUI.Hbyrid.Blazor;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
