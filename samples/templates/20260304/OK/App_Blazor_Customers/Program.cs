8d02b723bb84bb26c1d2aa520529af4a3ed00781
moljac <mcvjetko@holisticware.net>
2026-03-04 11:05:05 +0100

    OK implementation

---
+++ samples/templates/20260304/OK/App_Blazor_Customers/Program.cs
@@ -0,0 +1,29 @@
+using App_Blazor_Customers.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.UseStaticFiles();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapRazorPages();
+
+app.Run();
---
+++ samples/templates/20260304/OK/notes.md
@@ -0,0 +1,10 @@
+
+
+dotnet run --project App_Blazor_Contacts/App_Blazor_Contacts.csproj
+dotnet run --project App_Blazor_Customers/App_Blazor_Customers.csproj 
+dotnet run --project App_YARP_Proxy/App_YARP_Proxy.csproj 
+
+
+http://localhost:5000
+http://localhost:5001
+http://localhost:5002
