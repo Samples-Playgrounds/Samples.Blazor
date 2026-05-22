0c7d5e6f4f0e877464f488a8f6ae57c9bba9d566
moljac <mcvjetko@holisticware.net>
2026-05-22 22:13:47 +0200

    new API fixes

--- samples/blazor-web-and-maui-hybrid-shared-code/beth-massi-eilon-lipton/AppDemo.MobileHybrid.MAUI.Blazor/App.xaml.cs
+++ samples/blazor-web-and-maui-hybrid-shared-code/beth-massi-eilon-lipton/AppDemo.MobileHybrid.MAUI.Blazor/App.xaml.cs
@@ -6,6 +6,21 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
+		/*
+		 9>App.xaml.cs(9,3): 
+		 Warning CS0618 : 
+		 'Application.MainPage.set' is obsolete: 'This property is deprecated. Initialize your application by overriding 
+		 Application.CreateWindow rather than setting MainPage. To modify the root page in an active application, 
+		 use Windows[0].Page for applications with a single window. For applications with multiple windows, use 
+		 Application.Windows to identify and update the root page on the correct window.  Additionally, each element 
+		 features a Window property, accessible when it's part of the current window.'
+		   
 		MainPage = new MainPage();
+		*/
+	}
+	
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "AppDemo.MobileHybrid.MAUI.Blazor" };
 	}
 }
